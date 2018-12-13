a875afe23635260d5f99259637ec0abc6914142a
HaleyCai <caiyq602x@163.com>
2018-12-13 15:19:02 +0800

    entity层 User

---
+++ src/main/java/xmu/ghct/crm/entity/User.java
@@ -0,0 +1,77 @@
+package xmu.ghct.crm.entity;
+
+import java.io.Serializable;
+import java.math.BigInteger;
+
+/**
+ * @author caiyq
+ * User类对应数据库表的student,teacher两个类
+ */
+public class User implements Serializable {
+    private BigInteger id;
+    private String account;
+    private String password;
+    private String name;
+    private String email;
+    private boolean sex;//sex默认false为男性
+
+    public BigInteger getId() {
+        return id;
+    }
+
+    public void setId(BigInteger id) {
+        this.id = id;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isSex() {
+        return sex;
+    }
+
+    public void setSex(boolean sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", account='" + account + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", sex=" + sex +
+                '}';
+    }
+}
