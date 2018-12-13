d5fd57f911481e9f53734c4a64b9b9fb4a689e73
zm-huang <1792657687@qq.com>
2018-12-13 17:21:28 +0800

    添加创建课程方法；添加获取课程文件

--- src/main/java/xmu/ghct/crm/mapper/courseMapper.java
+++ src/main/java/xmu/ghct/crm/mapper/courseMapper.java
@@ -3,9 +3,14 @@ package xmu.ghct.crm.mapper;
 import org.apache.ibatis.annotations.Param;
 import xmu.ghct.crm.entity.Course;
 
+import java.math.BigInteger;
+import java.util.List;
+
 public class courseMapper {
 
     void insertCourseByUserId(@Param("course") Course course);
+
+    List<Course> listCourseByTeacherId(@Param("teacherId")BigInteger teacherId);
 
 
 }
