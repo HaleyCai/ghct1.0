60269fa96acd3770b6a9dd22c6564ab5432909ac
HaleyCai <caiyq602x@163.com>
2018-12-20 19:19:34 +0800

    增加seminar的get，set

--- src/main/java/xmu/ghct/crm/entity/Seminar.java
+++ src/main/java/xmu/ghct/crm/entity/Seminar.java
@@ -1,6 +1,5 @@
 package xmu.ghct.crm.entity;
 
-import javax.xml.crypto.Data;
 import java.io.Serializable;
 import java.math.BigInteger;
 import java.util.Date;
@@ -18,83 +17,85 @@ public class Seminar implements Serializable {
     private Date enrollStartTime;
     private Date enrollEndTime;
 
-
-    public void setEnrollStartTime(Date enrollStartTime) {
-        this.enrollStartTime = enrollStartTime;
-    }
-
-    public void setEnrollEndTime(Date enrollEndTime) {
-        this.enrollEndTime = enrollEndTime;
-    }
-
-    public Date getEnrollStartTime() {
-        return enrollStartTime;
+    public BigInteger getSeminarId() {
+        return seminarId;
     }
 
     public void setSeminarId(BigInteger seminarId) {
         this.seminarId = seminarId;
     }
 
-    public void setCourseId(BigInteger courseId) {
-        this.courseId = courseId;
-    }
-
-    public void setRoundId(BigInteger roundId) {
-        this.roundId = roundId;
-    }
-
-    public void setSeminarName(String seminarName) {
-        this.seminarName = seminarName;
-    }
-
-    public void setIntroduction(String introduction) {
-        this.introduction = introduction;
-    }
-
-    public void setMaxTeam(int maxTeam) {
-        this.maxTeam = maxTeam;
-    }
-
-    public void setVisible(int visible) {
-        this.visible = visible;
-    }
-
-    public void setSeminarSerial(int seminarSerial) {
-        this.seminarSerial = seminarSerial;
-    }
-
-    public BigInteger getSeminarId() {
-        return seminarId;
-    }
-
     public BigInteger getCourseId() {
         return courseId;
+    }
+
+    public void setCourseId(BigInteger courseId) {
+        this.courseId = courseId;
     }
 
     public BigInteger getRoundId() {
         return roundId;
     }
 
+    public void setRoundId(BigInteger roundId) {
+        this.roundId = roundId;
+    }
+
     public String getSeminarName() {
         return seminarName;
+    }
+
+    public void setSeminarName(String seminarName) {
+        this.seminarName = seminarName;
     }
 
     public String getIntroduction() {
         return introduction;
     }
 
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
+
     public int getMaxTeam() {
         return maxTeam;
     }
 
-    public int isVisible() {
+    public void setMaxTeam(int maxTeam) {
+        this.maxTeam = maxTeam;
+    }
+
+    public int getVisible() {
         return visible;
+    }
+
+    public void setVisible(int visible) {
+        this.visible = visible;
     }
 
     public int getSeminarSerial() {
         return seminarSerial;
     }
 
+    public void setSeminarSerial(int seminarSerial) {
+        this.seminarSerial = seminarSerial;
+    }
+
+    public Date getEnrollStartTime() {
+        return enrollStartTime;
+    }
+
+    public void setEnrollStartTime(Date enrollStartTime) {
+        this.enrollStartTime = enrollStartTime;
+    }
+
+    public Date getEnrollEndTime() {
+        return enrollEndTime;
+    }
+
+    public void setEnrollEndTime(Date enrollEndTime) {
+        this.enrollEndTime = enrollEndTime;
+    }
 
     @Override
     public String toString() {
@@ -111,5 +112,4 @@ public class Seminar implements Serializable {
                 ", enrollEndTime=" + enrollEndTime +
                 '}';
     }
-
 }
