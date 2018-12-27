838e113e9ebb1ae199fbed2e5fc5eeda517aa25b
GuoFangJie <1767301697@qq.com>
2018-12-27 12:38:08 +0800

    成功运行

--- src/main/java/xmu/ghct/crm/CrmApplication.java
+++ src/main/java/xmu/ghct/crm/CrmApplication.java
@@ -1,12 +1,9 @@
 package xmu.ghct.crm;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@MapperScan("xmu.ghct.crm.mapper")
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class CrmApplication {
 
     public static void main(String[] args) {
