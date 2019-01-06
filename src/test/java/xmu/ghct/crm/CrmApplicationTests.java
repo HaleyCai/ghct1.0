2d14f98391210f4381f8a11180d5c9cadfeda4b9
HaleyCai <caiyq602x@163.com>
2019-01-06 11:28:52 +0800

    修改websocket打包问题

--- src/test/java/xmu/ghct/crm/CrmApplicationTests.java
+++ src/test/java/xmu/ghct/crm/CrmApplicationTests.java
@@ -3,10 +3,11 @@ package xmu.ghct.crm;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = {CrmApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CrmApplicationTests {
 
     @Test
